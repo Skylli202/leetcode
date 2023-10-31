f52feee4821b2d649adc340f98fac2a6712339cc
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-10-31 16:00:22 -0700

    Refactor (#29)

--- problems/0029/index.test.ts
+++ problems/0029/index.test.ts
@@ -10,47 +10,42 @@ test('29. Divide Two Integers', () => {
 });
 
 function divide(dividend: number, divisor: number): number {
+	// Result of 2 ** 31
+	const max32bSignedInteger = 2_147_483_648;
+
 	dividend = Number(dividend.toFixed(0));
 	divisor = Number(divisor.toFixed(0));
 
 	const absDividend = Math.abs(dividend);
 	const absDivisor = Math.abs(divisor);
 
-	let resultIsNegative = false;
-	if (dividend < 0) {
-		resultIsNegative = !resultIsNegative;
-	}
-
-	if (divisor < 0) {
-		resultIsNegative = !resultIsNegative;
-	}
+	// Usage of a XOR
+	const resultIsNegative = (dividend < 0) !== (divisor < 0);
 
 	if (absDividend === absDivisor) {
 		return resultIsNegative ? -1 : 1;
 	}
 
 	let quotient = 0;
-	let absDividendCopy = absDividend;
 
 	if (absDivisor === 1) {
 		quotient = absDividend;
-		absDividendCopy -= absDividend;
-	}
-
-	while (absDividendCopy >= absDivisor) {
-		quotient += 1;
-		absDividendCopy -= absDivisor;
+	} else {
+		let absDividendCopy = absDividend;
+		while (absDividendCopy >= absDivisor) {
+			quotient += 1;
+			absDividendCopy -= absDivisor;
+		}
 	}
 
 	quotient = resultIsNegative ? -quotient : quotient;
 
-	console.log(quotient);
-	if (quotient > ((2 ** 31) - 1)) {
-		return (2 ** 31) - 1;
+	if (quotient > (max32bSignedInteger - 1)) {
+		return max32bSignedInteger - 1;
 	}
 
-	if (quotient < (-(2 ** 31))) {
-		return -(2 ** 31);
+	if (quotient < (-max32bSignedInteger)) {
+		return -max32bSignedInteger;
 	}
 
 	return quotient;
